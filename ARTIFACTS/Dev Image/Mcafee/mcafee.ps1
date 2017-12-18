931c8622d162670d7b05266dba95137e54037569
HASHIM890 <syedhashim1389@gmail.com>
2017-12-18 13:12:08 +0530

    Syed Hashim

---
+++ ARTIFACTS/Dev Image/Mcafee/mcafee.ps1
@@ -0,0 +1,39 @@
+function Create-Folder {
+    Param ([string]$path)
+    if ((Test-Path $path) -eq $false) 
+    {
+        Write-Host "$path doesn't exist. Creating now.."
+        New-Item -ItemType "directory" -Path $path
+    }
+}
+
+function Download-File{
+    Param ([string]$src, [string] $dst)
+
+    (New-Object System.Net.WebClient).DownloadFile($src,$dst)
+    #Invoke-WebRequest $src -OutFile $dst
+}
+
+function WaitForFile($File) {
+  while(!(Test-Path $File)) {    
+    Start-Sleep -s 10;   
+  }  
+} 
+
+
+#Setup Folders
+
+$setupFolder = "c:\Mcafee"
+Create-Folder "$setupFolder"
+
+
+if((Test-Path "$setupFolder\Mcafee.exe") -eq $false)
+{
+  
+        Download-File "https://mylibrary123.blob.core.windows.net/reposit/Mcafee.exe" "$setupFolder\Mcafee.exe"  
+}
+
+
+#Instalaation of exe
+
+Start-Process "$setupFolder\Mcafee.exe" -verbose
